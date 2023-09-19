5fde9d846e7c7292ee0b3364ceeeb5cfc6234252
SM-Saim <saim777m@example.com>
2023-09-19 19:20:42 +0600

    osts component added fro routing

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Users from "./components/Users/Users.jsx";
 import UserDetails from "./components/UserDetails/UserDetails.jsx";
+import Posts from "./components/Posts/Posts.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
         loader: ({ params }) =>
           fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
         element: <UserDetails></UserDetails>,
+      },
+      {
+        path: "/posts",
+        elements: <Posts></Posts>,
       },
     ],
   },
