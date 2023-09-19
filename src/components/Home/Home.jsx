2de84512b4af70f16efbbf4a362f4725a9fb4155
SM-Saim <saim777m@example.com>
2023-09-20 00:29:11 +0600

    useNavigation hook applied

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Header></Header>
+      {navigation.state === "loading" ? (
+        <p>Loading .....</p>
+      ) : (
+        <Outlet></Outlet>
+      )}
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
