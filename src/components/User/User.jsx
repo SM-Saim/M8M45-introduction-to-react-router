03b206ac2041a30eb1052b7ad061e62b5e7ecfe7
SM-Saim <saim777m@example.com>
2023-09-19 13:43:00 +0600

    user data style set

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,17 +1,9 @@
+import "./User.css";
 const User = ({ user }) => {
   const { name, phone, email, website } = user;
 
-  // applying style
-  const useStyle = {
-    border: "2px solid gray",
-    margin: "10px",
-    padding: "10px",
-    borderRadius: "5px",
-    backgroundColor: "cyan",
-  };
-
   return (
-    <div style={useStyle}>
+    <div className="useStyle ">
       <h2>{name}</h2>
       <p>Eamil: {email}</p>
       <p>Phone: {phone}</p>
